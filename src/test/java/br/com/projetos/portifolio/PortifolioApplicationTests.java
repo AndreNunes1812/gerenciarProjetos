adb57412a3592140028619039eb52e1c66b59637
Andre Nunes <andre.nunes020770@gmail.com>
2024-03-30 00:51:15 -0300

    criacao do projeto

---
+++ src/test/java/br/com/projetos/portifolio/PortifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetos.portifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
